e1757bcbff685db675d9928bf04d346ef0a38a4b
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-09-07 16:00:54 +0900

    認証できた

--- src/app/api/generate-google-oauth-url/route.ts
+++ src/app/api/generate-google-oauth-url/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   const oauth2Client = new google.auth.OAuth2({
     clientId: (process.env.NEXT_PUBLIC_CLIENT_ID || "").toString(),
     clientSecret: (process.env.NEXT_PUBLIC_CLIENT_SECRET || "").toString(),
-    redirectUri: "http://localhost:3000/callback",
+    redirectUri: (process.env.NEXT_PUBLIC_URL || "").toString() + "/callback",
   });
 
   const authorizeUrl = oauth2Client.generateAuthUrl({
